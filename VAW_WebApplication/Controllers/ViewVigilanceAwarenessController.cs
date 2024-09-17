2f513df7a56ae70f771e9522ad19fafbd6748a18
atul-cvc <atulk@tekskillsinc.net>
2024-09-17 16:40:02 +0530

    commit for views

---
+++ VAW_WebApplication/Controllers/ViewVigilanceAwarenessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VAW_WebApplication.Controllers
+{
+    public class ViewVigilanceAwarenessController : Controller
+    {
+        // GET: ViewVigilanceAwareness
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

44080371422dbc6e23a4b99063273c6836452cbb
ChandanKumar1983 <chandan.kumar0910@gmail.com>
2024-09-18 16:58:08 +0530

    commit for create building capacity

--- VAW_WebApplication/Controllers/ViewVigilanceAwarenessController.cs
+++ VAW_WebApplication/Controllers/ViewVigilanceAwarenessController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Util;
+using System.Xml.Linq;
 using VAW_BusinessAccessLayer;
+using VAW_WebApplication.Models;
 
 namespace VAW_WebApplication.Controllers
 {
@@ -24,10 +27,47 @@ namespace VAW_WebApplication.Controllers
             }
             return View();
         }
-
+        [HttpGet]
         public ActionResult CreateCapacityBuilding()
         {
-            return View();
+            Tran_a_1b_capacitybulidingprogram_ViewModel vmdata=new Tran_a_1b_capacitybulidingprogram_ViewModel();
+            vmdata.VAW_Year = "2014";
+            vmdata.CvoId = "CVO_SBI";
+            vmdata.CvoOrgCode = "I61";
+            vmdata.FromDate=DateTime.Now;
+            vmdata.ToDate=DateTime.Now;
+            vmdata.TrainingNameList = new List<SelectListItem> {
+            new SelectListItem { Value = "FRESH", Text = "Fresh Inductees" },
+            new SelectListItem { Value = "REFRESH", Text = "Refresher Course" }            
+        };
+            return View(vmdata);
+        }
+
+        [HttpPost]
+        public ActionResult CreateCapacityBuilding( Tran_a_1b_capacitybulidingprogram_ViewModel VmData )
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            Tran_a_1b_capacitybulidingprogram_ViewModel vmdata = new Tran_a_1b_capacitybulidingprogram_ViewModel();
+            vmdata.VAW_Year = "2014";
+            vmdata.CvoId = "CVO_SBI";
+            vmdata.CvoOrgCode = "I61";
+            vmdata.FromDate = DateTime.Now;
+            vmdata.ToDate = DateTime.Now;
+            vmdata.TrainingNameList = new List<SelectListItem> {
+            new SelectListItem { Value = "FRESH", Text = "Fresh Inductees" },
+            new SelectListItem { Value = "REFRESH", Text = "Refresher Course" }
+            };
+             return View(vmdata);
         }
     }
 }
